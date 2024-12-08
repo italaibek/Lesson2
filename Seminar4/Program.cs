eada34b8047dc46ffac9f014af9fb75a05a8fd5b
Talaibek Iskakov <talaibek.iskakov@gmail.com>
2024-12-08 22:27:34 +0600

    lesson2:seminar4

---
+++ Seminar4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+WriteLine("Введите число: ");
+int number = int.Parse(ReadLine());
+
+if ((number % 7 == 0) && (number % 23 == 0))
+{
+    WriteLine($"{number}->да");
+}
+else
+{
+    WriteLine($"{number}->нет");
+}
