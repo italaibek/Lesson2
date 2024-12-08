e45048632ead5b7312921f15faa76e72d1b7d15b
Talaibek Iskakov <talaibek.iskakov@gmail.com>
2024-12-08 21:26:19 +0600

    Lesson2:Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 5;
+int[] arr = { 2, 55, 7, 1, 73 };
+int i=0;
+int max = arr[0];
+while (i < n) {
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    i++;
+}
+Console.WriteLine(max);
+
