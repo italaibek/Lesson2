4cafe2d6ca9cd58060d2ee0631b2f011ee88d238
Talaibek Iskakov <talaibek.iskakov@gmail.com>
2024-12-08 22:21:01 +0600

    lesson2:seminar3

---
+++ Seminar3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+Console.Clear();
+Write("Введите первое число: ");
+int a = int.Parse(ReadLine());
+Write("Введите второе число: ");
+int b = int.Parse(ReadLine());
+
+if (a % b == 0)
+{
+    WriteLine("кратно");
+}
+else
+{
+    WriteLine("не кратно");
+}
