d9fd617e0f93ff6d897e12ff4df319c9d8e00d09
Talaibek Iskakov <talaibek.iskakov@gmail.com>
2024-12-08 22:43:31 +0600

    lesson2:seminar5

---
+++ Seminar5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введите А:");
+int A = int.Parse(Console.ReadLine());
+Write("Введите В:");
+int B = int.Parse(Console.ReadLine());
+
+if(B*B==A){
+    WriteLine("А является квадратом В");
+}
+else
+{
+    if(Math.Pow(A,2)==B)
+    {
+        WriteLine("B является квадратом А");
+    }
+    else{
+        WriteLine("B не является квадратом А");
+    }
+}
+
