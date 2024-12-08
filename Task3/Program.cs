1e3d0cd8d2ad09e107b45b1cb44090434f95515a
Talaibek Iskakov <talaibek.iskakov@gmail.com>
2024-12-08 21:13:17 +0600

    Lesson2:Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿//int n = 10;
+int[] array = { 2, 3, 5, 8, 7, 16, 6, 7, 32, 96 };
+int i = 0;
+//while(i<n){
+while(i<array.Length){
+    if(array[i]%2==0){
+        Console.Write(array[i] + " ");
+    }
+    i++;
+}
