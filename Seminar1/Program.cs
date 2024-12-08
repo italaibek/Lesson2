59cb95705a524b8b0ca4eae654e30e776024c515
Talaibek Iskakov <talaibek.iskakov@gmail.com>
2024-12-08 22:01:04 +0600

    lesson2:Seminar1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using static System.Console;
+
+Clear();
+int number = new Random().Next(10, 100);
+int a1 = number / 10;
+int a2 = number % 10;
+
+WriteLine(number);
+
+if(a1>a2){
+    WriteLine(a1);
+}
+else{
+    WriteLine(a2);
+}
