7df5fcfb86a23b200b283b30378d5493c426a858
Talaibek Iskakov <talaibek.iskakov@gmail.com>
2024-12-09 12:02:02 +0600

    lesson2:seminar7

---
+++ Seminar7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите номер четверти:");
+int n=int.Parse(ReadLine());
+
+if(n==1){
+    WriteLine("x>0 && y>0");
+}
+
+if(n==2){
+    WriteLine("x>0 && y>0");
+}
+
+if(n==3){
+    WriteLine("x<0 && y>0");
+}
+
+if(n==4){
+    WriteLine("3 четверть");
+}
+
+if (n == 4) {
+    WriteLine("x<0 && y<0");
+}
