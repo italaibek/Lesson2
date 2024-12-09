138b4132a652d2930c8a13cd7763c4b05d31c4e2
Talaibek Iskakov <talaibek.iskakov@gmail.com>
2024-12-09 11:26:20 +0600

    lesson2:dz2:zadacha15

---
+++ DZ2/Zadacha15/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+        int day = int.Parse(ReadLine());
+if (day == 6 || day == 7)
+{
+    Write($"{day}->да");
+}
+else if (day >= 1 && day <= 5)
+{
+    Write($"{day}->нет");
+}
+else
+{
+    Write("Введите корректное число!");
+}
+    
