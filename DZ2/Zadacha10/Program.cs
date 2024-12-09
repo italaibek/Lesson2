6c6367fd6fac6509ca6d351748305fd710e40d1a
Talaibek Iskakov <talaibek.iskakov@gmail.com>
2024-12-09 10:41:20 +0600

    lesson2:dz2:Zadacha10

---
+++ DZ2/Zadacha10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введите трехзначное число: ");
+        int number = int.Parse(ReadLine());
+
+        // Получаем вторую цифру числа
+        int secondDigit = (number / 10) % 10;
+
+WriteLine($"Число: {number} Вторая цифра числа: " + secondDigit);
+    
