74d293bfbe6092b4979c9ef22008179ba1a5ab3c
Talaibek Iskakov <talaibek.iskakov@gmail.com>
2024-12-09 19:07:09 +0600

    lesson2:seminar9

---
+++ Seminar9/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите N: ");
+int n=int.Parse(ReadLine());
+
+for(int i=1; i<=n;i++){
+    WriteLine($"\"{i}\" в квадрате \"{i * i}\" ");
+}
