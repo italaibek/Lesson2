3c3fb30ced1ade02e4b0df94d008578661de2893
Talaibek Iskakov <talaibek.iskakov@gmail.com>
2024-12-09 10:59:48 +0600

    lesson2:dz2:zadacha13

---
+++ DZ2/Zadacha13/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+
+Write("Введите число: ");
+      string chislo = ReadLine();
+        if  (chislo.Length < 3)
+        {
+            WriteLine("Третьей цифры нет");
+        }
+        else
+        {WriteLine(chislo[2]);
+            
+        }
