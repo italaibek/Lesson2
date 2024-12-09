9648bf0f2b74829885cd280a6ad8621c82f7b8f3
Talaibek Iskakov <talaibek.iskakov@gmail.com>
2024-12-09 11:53:48 +0600

    lesson2:seminar6

---
+++ Seminar6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите координату X:");
+int x=int.Parse(ReadLine());
+Write("Введите координату Y:");
+int y=int.Parse(ReadLine());
+
+if(x>0 && y>0){
+    WriteLine("1 четверть");
+}
+
+if(x>0 && y>0){
+    WriteLine("1 четверть");
+}
+
+if(x<0 && y>0){
+    WriteLine("2 четверть");
+}
+
+if(x<0 && y<0){
+    WriteLine("3 четверть");
+}
+
+if(x>0 && y<0){
+    WriteLine("4 четверть");
+}
